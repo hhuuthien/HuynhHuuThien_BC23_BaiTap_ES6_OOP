d30495d9346450a1928ad464e667474f7d145825
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-03-29 20:18:24 +0700

    add MonAn.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,35 @@
-var arrMonAn = [
-  { maMonAn: 1, tenMonAn: "Nước lẩu haidilao", giaTien: 100 },
-  { maMonAn: 2, tenMonAn: "Mì cay thành đô", giaTien: 200 },
-  { maMonAn: 3, tenMonAn: "Mực bạch ngọc", giaTien: 300 },
-];
+// var arrMonAn = [
+//   { maMonAn: 1, tenMonAn: "Nước lẩu haidilao", giaTien: 100 },
+//   { maMonAn: 2, tenMonAn: "Mì cay thành đô", giaTien: 200 },
+//   { maMonAn: 3, tenMonAn: "Mực bạch ngọc", giaTien: 300 },
+// ];
+
+import { MonAn } from "../models/MonAn.js";
+
+let arrayMonAn = [];
+
+let renderDanhSachMonAn = (arrayMonAn) => {
+  let stringHTML = "";
+  for (let monAn of arrayMonAn) {
+    let trHTML = `<tr>
+      <td>${monAn.maMonAn}</td>
+      <td>${monAn.tenMonAn}</td>
+      <td>${monAn.giaTien}</td>
+      <td><img src="${monAn.linkAnh}"/></td>
+    </tr>`;
+    stringHTML += trHTML;
+  }
+  document.querySelector(".baiTap1 .table .tblDanhMucMonAn").innerHTML = stringHTML;
+};
+
+let buttonThemMonAn = document.querySelector(".baiTap1 .card .btn-success");
+buttonThemMonAn.onclick = () => {
+  let monAn = new MonAn();
+  let arrayInput = document.querySelectorAll(".baiTap1 .card input");
+  for (let input of arrayInput) {
+    let { id, value } = input;
+    monAn[id] = value;
+  }
+  arrayMonAn.push(monAn);
+  renderDanhSachMonAn(arrayMonAn);
+};
---
+++ models/MonAn.js
@@ -0,0 +1,8 @@
+export class MonAn {
+  maMonAn = "";
+  tenMonAn = "";
+  giaTien = 0;
+  linkAnh = "";
+
+  constructor() {}
+}
