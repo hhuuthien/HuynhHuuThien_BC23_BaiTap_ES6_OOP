973b7e10e74cc4ba1d22b4635c26988d18e8743a
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-03-30 23:37:21 +0700

    render menu baiTap2

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,3 @@
-// var arrMonAn = [
-//   { maMonAn: 1, tenMonAn: "Nước lẩu haidilao", giaTien: 100 },
-//   { maMonAn: 2, tenMonAn: "Mì cay thành đô", giaTien: 200 },
-//   { maMonAn: 3, tenMonAn: "Mực bạch ngọc", giaTien: 300 },
-// ];
-
 import { MonAn } from "../models/MonAn.js";
 import { Menu } from "../models/Menu.js";
 
@@ -31,3 +25,33 @@ window.xoaMon = (maMon) => {
   menu.render(".baiTap1 .table .tblDanhMucMonAn");
   menu.luuLocalStorage();
 };
+
+//
+//
+// Bài tập 2
+//
+//
+
+var arrMonAn = [
+  { maMonAn: 1, tenMonAn: "Nước lẩu haidilao", giaTien: 100 },
+  { maMonAn: 2, tenMonAn: "Mì cay thành đô", giaTien: 200 },
+  { maMonAn: 3, tenMonAn: "Mực bạch ngọc", giaTien: 300 },
+];
+
+let renderMenu = (arrMonAn) => {
+  let stringHTML = "";
+  for (let monAn of arrMonAn) {
+    stringHTML += `<div class="row mt-3">
+      <div class="col-3">${monAn.maMonAn}</div>
+      <div class="col-3">${monAn.tenMonAn}</div>
+      <div class="col-3">${monAn.giaTien}</div>
+      <div class="col-3">
+        <button class="btn btn-danger">+</button>
+        <button class="btn btn-danger">-</button>
+      </div>
+    </div>`;
+  }
+  document.querySelector(".baiTap2 .card .card-body").innerHTML += stringHTML;
+};
+
+renderMenu(arrMonAn);
